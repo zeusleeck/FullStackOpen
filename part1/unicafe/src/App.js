fef0bd8c852afeea1237792ae065499990c596b9
Chong Kai Lee <chongkailee@Chongs-MacBook-Air.local>
2022-09-07 23:33:17 +0800

    Completed exercise 1.10

--- part1/unicafe/src/App.js
+++ part1/unicafe/src/App.js
@@ -168,7 +168,7 @@ const App = () => {
 }*/
 
 //exercise 1.9
-const Statistics = (props) => {
+/*const Statistics = (props) => {
   let {good,neutral,bad,all} = props
   if (good, neutral, bad, all){
     return(
@@ -223,6 +223,68 @@ const App = () => {
       </div>
       <h1>statistics</h1>
       <Statistics good={good} neutral={neutral} bad={bad} all={all} />
+    </div>
+  )
+}*/
+
+//exercise 1.10
+const Button = ({ handleClick, text }) => (
+  <button onClick={handleClick}>{text}</button>
+);
+
+const StatisticLine = (props) => {
+  const {text, value} = props 
+  return(
+    <div>
+      <tr><td>{text} {value} </td></tr>
+    </div>
+  )
+}
+
+const Statistics = (props) => {
+  let {good, neutral, bad, all, average, positive, clicks} = props
+  if (clicks){
+    return(
+      <div>
+        <StatisticLine text="good" value ={good} />
+        <StatisticLine text="neutral" value ={neutral} />
+        <StatisticLine text="bad" value ={bad} />
+        <StatisticLine text="all" value ={all} />
+        <StatisticLine text="average" value ={average} />
+        <StatisticLine text="positive" value ={positive + '%'} />
+      </div>
+    )
+  }
+  return(
+    <div>
+      <p>No feedback given</p>
+    </div>
+  )
+}
+
+const App = () => {
+  // save clicks of each button to its own state
+  const [good, setGood] = useState(0)
+  const [neutral, setNeutral] = useState(0)
+  const [bad, setBad] = useState(0)
+  const [clicks, allClicks] = useState(0)
+
+  const all = good + neutral + bad;
+  let average = (good - bad) / all
+  let positive = (good / all) * 100
+
+  return (
+    <div>
+      <div>
+        <h1>give feedback</h1>
+
+
+        <Button handleClick={() => [setGood(good+1), allClicks(clicks+1)]} text="good" />
+        <Button handleClick={() => [setNeutral(neutral+1),allClicks(clicks+1)]} text="neutral" />
+        <Button handleClick={() => [setBad(bad+1),allClicks(clicks+1)]} text="bad" />
+      </div>
+      <h1>statistics</h1>
+      <Statistics good={good} neutral={neutral} bad={bad} all={all} average={average} positive={positive} clicks={clicks}/>
     </div>
   )
 }
