24a7ea2310efde7fb32a34200337f452806bafd0
Chong Kai <zeusleeck@gmail.com>
2022-11-10 22:01:12 +0800

    Completed exercise 2.14

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -4,75 +4,85 @@ import { useState , useEffect} from 'react'
 const api_key = process.env.REACT_APP_API_KEY
 
 const DisplayOne = ({countrieslist}) => {
-  const [weathers, setWeathers] = useState('')
+  const [weathers, setWeathers] = useState([])
   const flag = countrieslist.map(country => country.flags.png)
   const capital = countrieslist.map(country => country.capital[0])
   
   useEffect(() => {
-    const params = new URLSearchParams([['q', capital], ['appid', process.env.REACT_APP_API_KEY], ['units', "Celsius"]]);
+    const params = new URLSearchParams([['q', capital], ['appid', api_key], ['units', "metric"]]);
     axios.get("http://api.openweathermap.org/data/2.5/weather", {params})
     .then(response => {
-      console.log('promise fulfilled 2')
       setWeathers(response.data)
     })
-  }, [])
+  },[])
   
-  return(
-    <table id = "one_country">
-      <thead><tr><td><h1>{countrieslist.map(country => country.name.common)}</h1></td></tr></thead>
-      <tbody>
-        <tr>
-          <td>
-            <p>
-              capital {countrieslist.map(country => country.capital[0])} <br/>
-              area {countrieslist.map(country => country.area)}
-            </p>
-          </td> 
-        </tr>
-        <tr>
-          <td>
-            <h2>languages:</h2>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <ul>
-              {countrieslist.map(country => { 
-                  return(
-                      Object.keys(country.languages).map((languagekey) => {
-                        return(
-                          <li key = {languagekey}>{country.languages[languagekey]}</li>
-                        )}
+  return (
+    <>
+      {weathers.main ? (
+        <table id = "one_country">
+        <thead><tr><td><h1>{countrieslist.map(country => country.name.common)}</h1></td></tr></thead>
+        <tbody>
+          <tr>
+            <td>
+              <p>
+                capital {countrieslist.map(country => country.capital[0])} <br/>
+                area {countrieslist.map(country => country.area)}
+              </p>
+            </td> 
+          </tr>
+          <tr>
+            <td>
+              <h2>languages:</h2>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <ul>
+                {countrieslist.map(country => { 
+                    return(
+                        Object.keys(country.languages).map((languagekey) => {
+                          return(
+                            <li key = {languagekey}>{country.languages[languagekey]}</li>
+                          )}
+                        )
                       )
-                    )
-                  }
-                )
-              }
-            </ul>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <img src={flag} alt = "flags"/>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            <h2>Weather in {capital}</h2>
-          </td>
-        </tr>
-        <tr>
-          <td>
-            tempurature {weathers.main?.temp} Celcius
-          </td>
-        </tr>
-        <tr>
-          <td>tempurature {weathers.wind?.speed} m/s</td>
-        </tr>
-      </tbody>
-    </table>
-  )
-}
+                    }
+                  )
+                }
+              </ul>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img src={flag} alt = "flags"/>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <h2>Weather in {capital}</h2>
+            </td>
+          </tr>
+          <tr>
+            <td>
+              tempurature {weathers.main.temp} Celcius
+            </td>
+          </tr>
+          <tr>
+            <td>
+              <img alt="weather icon" src={`http://openweathermap.org/img/wn/${weathers?.weather[0].icon}@2x.png`}/>
+            </td>
+          </tr>
+          <tr>
+            <td>tempurature {weathers.wind.speed} m/s</td>
+          </tr>
+        </tbody>
+      </table>
+      ): null }
+    </>
+  );
+};
+
+  
 
 const FilterCountries = (props) =>{
   const {filterCountry, countries, setCountries} = props
